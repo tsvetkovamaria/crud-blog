a4d333265dbadc7161b8d34741e3ca7f73f52c14
Maria Tsvetkova <tsvetkovamariia@gmail.com>
2017-04-10 14:34:18 +0600

    create and read post

---
+++ readme.md
@@ -0,0 +1,6 @@
+This is a simple crud app to get familiar with express, mongo and node.js
+
+To run app in developers mode
+
+$ npm run dev
+
---
+++ routes/index.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var router = express.Router();
+const mongo_client = require('mongodb').MongoClient;
+var db;
+var db_link = 'mongodb://localhost:27017/blog'
+mongo_client.connect(, function (err, database) {
+  if (err) throw err
+  db = database;
+})
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+	db.collection('articles').find().toArray(function(err, results) {
+		res.render('index', { title: 'Blog', articles: results});
+	})
+});
+
+router.get('/new', function(req, res, next) {
+	res.render('new', { title: 'Blog'});
+});
+
+router.post('/blog', function(req, res){
+	var collection = db.collection('articles');
+	collection.insert(req.body, function(err, result) {
+		if (err) return console.log(err)
+   		res.redirect('/');
+    })
+});	
+
+module.exports = router;
